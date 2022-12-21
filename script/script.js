eb444cb3bb7b5807760b8db2f7ae14c1689e8503
francescotrudu <francescotrudu84@gmail.com>
2022-12-21 15:40:09 +0100

    Add links via cdn with axion

---
+++ script/script.js
@@ -0,0 +1,21 @@
+const { createApp } = Vue;
+
+createApp({
+    data() {
+        return {
+        }
+    },
+
+    // methods
+    methods: {
+
+        
+    },
+
+}).mount('#app')
+
+axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+    .then(function (response){
+        const result = response.data;
+        console.log(result)
+    });
