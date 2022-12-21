9e465c3902651371333cd5747fcef262f46ef591
francescotrudu <francescotrudu84@gmail.com>
2022-12-21 16:06:51 +0100

    Show on html a random email when reload page

--- script/script.js
+++ script/script.js
@@ -3,19 +3,28 @@ const { createApp } = Vue;
 createApp({
     data() {
         return {
+            randomEmail: null,
         }
     },
 
     // methods
     methods: {
+        getRandomEmail() {
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+            // arrow function reference 'this' parent
+                .then( (response) => {
+                    const result = response.data.response;
+                    this.randomEmail = response.data.response
+                    console.log(this)
+                });
+        },
 
         
     },
+    //call function at every reload page
+    created() {
+        this.getRandomEmail();
+    }
 
 }).mount('#app')
 
-axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-    .then(function (response){
-        const result = response.data;
-        console.log(result)
-    });
